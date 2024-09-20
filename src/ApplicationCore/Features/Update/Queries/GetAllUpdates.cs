525003eb35dcd128be922f0fd58823d237b6a3d2
Alonso Castillo <miguel.ocampog@gmail.com>
2024-09-20 12:05:06 -0600

    se lee sp GetAvailableUpdates

---
+++ src/ApplicationCore/Features/Update/Queries/GetAllUpdates.cs
@@ -0,0 +1,73 @@
+﻿using System.Data;
+using Dapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using ApplicationCore.Common.Abstractions.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Features.Update.Queries
+{
+    public class GetAllUpdates
+    {
+        public class Response
+        {
+            public int TaskId { get; set; }
+            public int StoreDataId { get; set; }
+            public bool IsChained { get; set; }
+            public string StoreIp { get; set; }
+            public bool HasSlowInternet { get; set; }
+            public string ThisIp { get; set; }
+            public int ReleaseTaskTypeId { get; set; }
+            public bool HighPriority { get; set; }
+            public int ExpirationDays { get; set; }
+        }
+
+        public class Query : IRequest<List<Response>>
+        {
+            public string StoreName { get; set; }
+            public int StoreDataId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, List<Response>>
+        {
+            private readonly IDbConnectionFactory _dbConnectionFactory;
+            private readonly ILogger<Handler> _logger;
+
+            public Handler(IDbConnectionFactory dbConnectionFactory, ILogger<Handler> logger)
+            {
+                _dbConnectionFactory = dbConnectionFactory;
+                _logger = logger;
+            }
+
+            public async Task<List<Response>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                using IDbConnection connection = _dbConnectionFactory.CreateConnection("defaultConnection");
+
+                try
+                {
+                    var result = await connection.QueryAsync<Response>(
+                        "GetAvailableUpdates",
+                        new { 
+                            StoreName = request.StoreName,
+                            Type = 1,
+                            TaskId = 0,
+                            StoreDataId = request.StoreDataId,
+                        },
+                        commandType: CommandType.StoredProcedure
+                    );
+
+                    return result.ToList();
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error executing stored procedure GetAvailableUpdates");
+                    throw;
+                }
+            }
+
+        }
+    }
+}
---
+++ src/WebApi/Controllers/UpdateController.cs
@@ -0,0 +1,31 @@
+﻿using ApplicationCore.Features.Update.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class UpdateController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UpdateController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet, Route("GetAllUpdates/{storeName}/{storeDataId?}")]
+    public async Task<IActionResult> GetAllUpdates(string storeName, int storeDataId = 0)
+    {
+        var result = await _mediator.Send(new GetAllUpdates.Query
+        {
+            StoreName = storeName,
+            StoreDataId = storeDataId
+        });
+
+        return Ok(result);
+    }
+}
